b3ff72b78a7cae6c0018f3eefed64a1a4ca8294e
Sagnik1Majumder <88670893+Sagnik1Majumder@users.noreply.github.com>
2021-08-22 11:54:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var runner,run
+var black,road
+
+function preload() {
+  run = loadAnimation("Runner-1.png", "Runner-2.png");
+  
+
+  road = loadAnimation("path.png")
+}
+
+function setup() {
+  createCanvas(1300, 600);
+  
+  black = createSprite(650,180,400,20);
+  black.addAnimation("cake",road);
+  black.velocityY = 4;
+  
+  runner = createSprite(650,300,20,50);
+  runner.addAnimation("running", run);
+  runner.scale=0.1
+  
+}
+
+function draw() {
+  background(220);
+
+ drawSprites()
+
+  if(black.y>400){
+    black.y = height/2
+  }}
